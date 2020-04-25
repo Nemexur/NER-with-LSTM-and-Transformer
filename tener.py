2c6051db37d58f777348be08d8215bef99898ab3
nemexur <alex.milogradsky@gmail.com>
2020-04-25 23:15:49 +0300

    Небольшие правки

--- tener.py
+++ tener.py
@@ -29,14 +29,17 @@ class PositionalEncodingTENER(torch.nn.Module):
         super().__init__()
         self._input_dim = input_dim
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        batch_size, seq_len = x.size()
-        return self._get_pos_embeddings(2 * seq_len)
+    def forward(
+        self,
+        x: torch.Tensor
+    ) -> torch.Tensor:
+        batch_size, timesteps = x.size()
+        return self._get_pos_embeddings(2 * timesteps)
 
-    def _get_pos_embeddings(self, seq_len: int):
+    def _get_pos_embeddings(self, timesteps: int):
         half_dim = self._input_dim // 2
-        positional_encoding = torch.zeros(seq_len, self._input_dim, requires_grad=False)
-        position = torch.arange(-seq_len // 2, seq_len // 2, dtype=torch.float).unsqueeze(1)
+        positional_encoding = torch.zeros(timesteps, self._input_dim, requires_grad=False)
+        position = torch.arange(-timesteps // 2, timesteps // 2, dtype=torch.float).unsqueeze(1)
         div_term = torch.exp(
             torch.arange(half_dim, dtype=torch.float) * -(math.log(10000) / (half_dim - 1))
         )
@@ -44,7 +47,7 @@ class PositionalEncodingTENER(torch.nn.Module):
             [torch.sin(position * div_term.unsqueeze(0)),
              torch.cos(position * div_term.unsqueeze(0))],
             dim=1
-        ).view(seq_len, -1)
+        ).view(timesteps, -1)
         return positional_encoding
 
 
@@ -52,26 +55,33 @@ class PositionwiseFeedForward(torch.nn.Module):
     """Implements FFN equation."""
     def __init__(self, input_dim: int, ff_dim: int, dropout: float = 0.1) -> None:
         super().__init__()
-        self.w_1 = torch.nn.Linear(input_dim, ff_dim)
-        self.w_2 = torch.nn.Linear(ff_dim, input_dim)
-        self.dropout = torch.nn.Dropout(dropout)
+        self._w_1 = torch.nn.Linear(input_dim, ff_dim)
+        self._w_2 = torch.nn.Linear(ff_dim, input_dim)
+        self._dropout = torch.nn.Dropout(dropout)
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+    def forward(
+        self,
+        x: torch.Tensor
+    ) -> torch.Tensor:
+        return self._w_2(self._dropout(F.relu(self._w_1(x))))
 
 
 class TransformerEncoder(torch.nn.Module):
     """Core encoder is a stack of N layers"""
     def __init__(self, layer: torch.nn.Module, num_layers: int) -> None:
         super().__init__()
-        self.layers = util.clone(layer, num_layers)
-        self.norm = LayerNorm(layer.size)
+        self._layers = util.clone(layer, num_layers)
+        self._norm = LayerNorm(layer._size)
 
-    def forward(self, x, mask):
+    def forward(
+        self,
+        x: torch.Tensor,
+        mask: torch.Tensor
+    ) -> torch.Tensor:
         """Pass the input (and mask) through each layer in turn."""
-        for layer in self.layers:
+        for layer in self._layers:
             x = layer(x, mask)
-        return self.norm(x)
+        return self._norm(x)
 
 
 class SublayerConnection(torch.nn.Module):
@@ -81,36 +91,46 @@ class SublayerConnection(torch.nn.Module):
     """
     def __init__(self, size: int, dropout: float) -> None:
         super().__init__()
-        self.norm = LayerNorm(size)
-        self.dropout = torch.nn.Dropout(dropout)
+        self._norm = LayerNorm(size)
+        self._dropout = torch.nn.Dropout(dropout)
 
-    def forward(self, x: torch.Tensor, sublayer: Callable[[torch.Tensor], torch.Tensor]) -> torch.Tensor:
+    def forward(
+        self,
+        x: torch.Tensor,
+        sublayer: Callable[[torch.Tensor], torch.Tensor]
+    ) -> torch.Tensor:
         """Apply residual connection to any sublayer with the same size."""
-        return x + self.dropout(sublayer(self.norm(x)))
+        return x + self._dropout(sublayer(self._norm(x)))
 
 
 class EncoderLayer(torch.nn.Module):
     """
-    Encoder is made up of self-attention and feed forward
+    Encoder is made up of self-attention and feedforward
     both followed by Residual Connection.
     """
-    def __init__(self,
-                 size: int,
-                 self_attn: torch.nn.Module,
-                 feed_forward: torch.nn.Module,
-                 dropout: float) -> None:
+    def __init__(
+        self,
+        size: int,
+        self_attn: torch.nn.Module,
+        feed_forward: torch.nn.Module,
+        dropout: float
+    ) -> None:
         super().__init__()
-        self.self_attn = self_attn
-        self.feed_forward = feed_forward
-        self.sublayer = util.clone(SublayerConnection(size, dropout), 2)
-        self.size = size
+        self._self_attn = self_attn
+        self._feed_forward = feed_forward
+        self._sublayer = util.clone(SublayerConnection(size, dropout), 2)
+        self._size = size
 
-    def forward(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
-        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
-        return self.sublayer[1](x, self.feed_forward)
+    def forward(
+        self,
+        x: torch.Tensor,
+        mask: torch.Tensor
+    ) -> torch.Tensor:
+        x = self._sublayer[0](x, lambda x: self._self_attn(x, x, x, mask))
+        return self._sublayer[1](x, self._feed_forward)
 
 
-class MultiHeadedAttention(torch.nn.Module):
+class MultiHeadSelfAttentionTENER(torch.nn.Module):
     """
     This class implements the key-value scaled dot product attention mechanism
     detailed in the paper `Attention is all you Need` but with some changes
@@ -141,50 +161,52 @@ class MultiHeadedAttention(torch.nn.Module):
     ) -> None:
         super().__init__()
         assert input_dim % num_heads == 0, "input_dim must be a multiple of num_heads"
-        self.input_dim = input_dim
-        self.attention_dim = input_dim // num_heads
-        self.num_heads = num_heads
-        self.values_dim = values_dim or input_dim
-        self.query_linear = torch.nn.Linear(input_dim, input_dim)
-        self.value_linear = torch.nn.Linear(input_dim, values_dim)
-        self.output_projection = torch.nn.Linear(values_dim, input_dim)
-        self.dropout = torch.nn.Dropout(p=dropout)
+        self._input_dim = input_dim
+        self._attention_dim = input_dim // num_heads
+        self._num_heads = num_heads
+        self._values_dim = values_dim or input_dim
+        self._query_linear = torch.nn.Linear(input_dim, input_dim)
+        self._value_linear = torch.nn.Linear(input_dim, values_dim)
+        self._output_projection = torch.nn.Linear(values_dim, input_dim)
+        self._dropout = torch.nn.Dropout(p=dropout)
         # Biases
-        self.v_bias = torch.nn.Parameter(
-            torch.nn.init.xavier_normal_(torch.zeros(self.num_heads, self.attention_dim))
+        self._v_bias = torch.nn.Parameter(
+            torch.nn.init.xavier_normal_(torch.zeros(self._num_heads, self._attention_dim))
         )
-        self.position = PositionalEncodingTENER(self.attention_dim)
+        self._position = PositionalEncodingTENER(self._attention_dim)
 
-    def forward(self,
-                query: torch.Tensor,
-                key: torch.Tensor,
-                value: torch.Tensor,
-                mask: torch.Tensor = None) -> torch.Tensor:
+    def forward(
+        self,
+        query: torch.Tensor,
+        key: torch.Tensor,
+        value: torch.Tensor,
+        mask: torch.Tensor = None
+    ) -> torch.Tensor:
         batch_size = query.size(0)
         # 0) Relative positional encoding
-        position_emb = self.position(mask)
+        position_emb = self._position(mask)
         # 1) Do all the linear projections in batch from d_model => h x d_k
-        key = key.view(batch_size, -1, self.num_heads, self.attention_dim).transpose(1, 2)
-        query = self.query_linear(query).view(
-            batch_size, -1, self.num_heads, self.attention_dim
+        key = key.view(batch_size, -1, self._num_heads, self._attention_dim).transpose(1, 2)
+        query = self._query_linear(query).view(
+            batch_size, -1, self._num_heads, self._attention_dim
         ).transpose(1, 2)
-        value = self.value_linear(value).view(
-            batch_size, -1, self.num_heads, self.values_dim // self.num_heads
+        value = self._value_linear(value).view(
+            batch_size, -1, self._num_heads, self._values_dim // self._num_heads
         ).transpose(1, 2)
         # 2) Make all elements in equation 18 (except u * relative, it is not needed)
         query_key = torch.einsum('bnqd,bnkd->bnqk', query, key)
-        v_bias_relative = torch.einsum('nd,ld->nl', self.v_bias, position_emb)[None, :, None]
+        v_bias_relative = torch.einsum('nd,ld->nl', self._v_bias, position_emb)[None, :, None]
         query_relative = torch.einsum('bnqd,ld->bnql', query, position_emb)
         query_rel_and_v_bias_rel = query_relative + v_bias_relative
         # 3) Shift last dimension
         query_rel_and_v_bias_rel = self._shift(query_rel_and_v_bias_rel)
         attn = query_key + query_rel_and_v_bias_rel
         # 4) Masked softmax
-        x = self.masked_softmax(attn, value, batch_size=batch_size)
+        x = self._masked_softmax(attn, value, batch_size=batch_size)
         # 5) One more linear projection
-        return self.output_projection(x)
+        return self._output_projection(x)
 
-    def masked_softmax(
+    def _masked_softmax(
         self,
         attn: torch.Tensor,
         value: torch.Tensor,
@@ -194,27 +216,27 @@ class MultiHeadedAttention(torch.nn.Module):
         dropout: Callable = None
     ) -> torch.Tensor:
         if should_scale:
-            attn = attn / math.sqrt(self.attention_dim)
+            attn = attn / math.sqrt(self._attention_dim)
         if mask is not None:
             attn = attn.masked_fill(mask[:, None, None, :].eq(0), float('-inf'))
         attn = F.softmax(attn, dim=-1)
         if dropout is not None:
             attn = dropout(attn)
-        return torch.matmul(attn, value).transpose(1, 2).reshape(batch_size, -1, self.values_dim)
+        return torch.matmul(attn, value).transpose(1, 2).reshape(batch_size, -1, self._values_dim)
 
     def _shift(self, tensor):
         """
         Shift dimensions.
-        Input tensor: batch_size x num_heads x seq_len x 2seq_len.
-        Return tensor: batch_size x num_heads x seq_len x seq_len.
+        Input tensor: batch_size x num_heads x timesteps x 2 * timesteps.
+        Return tensor: batch_size x num_heads x timesteps x timesteps.
         """
-        batch_size, num_heads, seq_len, _ = tensor.size()
-        zero_pad = tensor.new_zeros(batch_size, num_heads, seq_len, 1)
-        # tensor ~ batch_size x num_heads x (2seq_len+1) x seq_len
-        tensor = torch.cat([tensor, zero_pad], dim=-1).view(batch_size, num_heads, -1, seq_len)
-        # tensor ~ batch_size x num_heads x 2seq_len x seq_len
-        tensor = tensor[:, :, :-1].view(batch_size, num_heads, seq_len, -1)
-        tensor = tensor[:, :, :, seq_len:]
+        batch_size, num_heads, timesteps, _ = tensor.size()
+        zero_pad = tensor.new_zeros(batch_size, num_heads, timesteps, 1)
+        # tensor ~ batch_size x num_heads x (2 * timesteps + 1) x timesteps
+        tensor = torch.cat([tensor, zero_pad], dim=-1).view(batch_size, num_heads, -1, timesteps)
+        # tensor ~ batch_size x num_heads x 2 * timesteps x timesteps
+        tensor = tensor[:, :, :-1].view(batch_size, num_heads, timesteps, -1)
+        tensor = tensor[:, :, :, timesteps:]
         return tensor
 
 
@@ -227,7 +249,7 @@ def make_model(
     dropout: float = 0.1
 ) -> TransformerEncoder:
     """`Helper`: Construct a model from hyperparameters."""
-    attn = MultiHeadedAttention(
+    attn = MultiHeadSelfAttentionTENER(
         heads, input_size, values_dim, dropout
     )
     ff = PositionwiseFeedForward(input_size, hidden_size, dropout)
@@ -275,41 +297,48 @@ class TENER(Seq2SeqEncoder):
         input_dropout: float = None,
     ) -> None:
         super().__init__()
-        self.transformer_layers = num_layers
-        self.num_layers = num_layers
-        self.output_projection_dim = output_projection_dim or input_dim
-        self.model = make_model(
+        self._transformer_layers = num_layers
+        self._num_layers = num_layers
+        self._output_projection_dim = output_projection_dim or input_dim
+        self._model = make_model(
             input_size=input_dim,
             hidden_size=hidden_dim,
             num_layers=num_layers,
             values_dim=values_dim,
             dropout=dropout
         )
-        self.input_dim = input_dim
-        self.output_dim = input_dim
-        self.output_projection = torch.nn.Linear(
-            self.input_dim,
-            self.output_projection_dim
+        self._input_dim = input_dim
+        self._output_dim = input_dim
+        self._output_projection = torch.nn.Linear(
+            self._input_dim,
+            self._output_projection_dim
         )
         if input_dropout:
-            self.dropout = torch.nn.Dropout(input_dropout)
+            self._dropout = torch.nn.Dropout(input_dropout)
         else:
-            self.dropout = lambda x: x
-        self.should_log_activations = False
+            self._dropout = lambda x: x
+        self._should_log_activations = False
 
-    def forward(self, token_embeddings: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
-        token_embeddings = self.dropout(token_embeddings)
-        model_output = self.model(token_embeddings, mask)
-        return self.output_projection(model_output)
+    def forward(
+        self,
+        inputs: torch.Tensor,
+        mask: torch.Tensor
+    ) -> torch.Tensor:
+        batch_size, timesteps, _ = inputs.size()
+        if mask is None:
+            mask = inputs.new_ones(batch_size, timesteps)
+        inputs = self._dropout(inputs)
+        model_output = self._model(inputs, mask)
+        return self._output_projection(model_output)
 
     def get_regularization_penalty(self):
         return 0.
 
     def get_input_dim(self) -> int:
-        return self.input_dim
+        return self._input_dim
 
     def get_output_dim(self) -> int:
-        return self.output_dim
+        return self._output_dim
 
     def is_bidirectional(self) -> bool:
         return True
